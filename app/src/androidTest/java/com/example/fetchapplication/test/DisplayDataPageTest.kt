9901de31818f426826926364b3125c689c751813
Colliquake <aroh002@ucr.edu>
2023-11-15 10:50:36 -0800

    added more testing for GetData and added tests for DisplayDataPage

--- app/src/androidTest/java/com/example/fetchapplication/test/DisplayDataPageTest.kt
+++ app/src/androidTest/java/com/example/fetchapplication/test/DisplayDataPageTest.kt
@@ -8,56 +8,53 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.isClickable
-import androidx.test.espresso.matcher.ViewMatchers.isEnabled
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.hasChildCount
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.fetchapplication.GetDataFragment
 import com.example.fetchapplication.MainActivity
-import org.hamcrest.Matchers.containsString
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class GetDataPageTest {
+class DisplayDataPageTest {
     @get:Rule()
     val activity = ActivityScenarioRule(MainActivity::class.java)
     
     @Test
-    fun ButtonHasGetDataText(){
+    fun CollapsedListSize() {
+        
+        //load GetDataFragment first so the coroutine (data retrieval) works
         val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
-        val getDataScenario = launchFragmentInContainer<GetDataFragment>()
-        
-        onView(ViewMatchers.withId(com.example.fetchapplication.R.id.get_data_button)).check(matches(withText(containsString("Get Data"))))
-    }
-    
-    @Test
-    fun ButtonIsClickable(){
-        onView(ViewMatchers.withId(com.example.fetchapplication.R.id.get_data_button)).check(matches(isClickable()))
-    }
-    
-    @Test
-    fun ButtonIsEnabled(){
-        onView(ViewMatchers.withId(com.example.fetchapplication.R.id.get_data_button)).check(matches(
-            isEnabled()
-        ))
-    }
-    
-    @Test
-    fun ButtonClickNavigatesToDisplayDataPage(){
-        val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
-        
-        val getDataScenario = launchFragmentInContainer<GetDataFragment>()
-        
-        getDataScenario.onFragment{ fragment ->
+        val getDataPageScenario = launchFragmentInContainer<GetDataFragment>()
+        getDataPageScenario.onFragment { fragment ->
             navController.setGraph(com.example.fetchapplication.R.navigation.nav_graph)
             Navigation.setViewNavController(fragment.requireView(), navController)
         }
-        
         onView(ViewMatchers.withId(com.example.fetchapplication.R.id.get_data_button)).perform(
-            ViewActions.click())
-        assert(navController.currentDestination?.id == com.example.fetchapplication.R.id.displayDataFragment)
+            ViewActions.click()
+        )
+        
+        onView(ViewMatchers.withId(com.example.fetchapplication.R.id.display_data_recycler_view)).check(
+            matches(
+                hasChildCount(4)
+            )
+        )
+    }
+    
+    @Test
+    fun CollapsedButtonsText() {
+        val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
+        val getDataPageScenario = launchFragmentInContainer<GetDataFragment>()
+        getDataPageScenario.onFragment { fragment ->
+            navController.setGraph(com.example.fetchapplication.R.navigation.nav_graph)
+            Navigation.setViewNavController(fragment.requireView(), navController)
+        }
+        onView(ViewMatchers.withId(com.example.fetchapplication.R.id.get_data_button)).perform(
+            ViewActions.click()
+        )
+        
+        
     }
 }
